7be0260ff6f835f0613c867f2b08923b73779661
Wei FeiYang <wesley.f.young@outlook.com>
2025-11-17 10:54:07 +0800

    feat(types): add metainfo fields for market face segment

--- packages/types/src/message.ts
+++ packages/types/src/message.ts
@@ -95,6 +95,10 @@ export const IncomingSegment = z.discriminatedUnion('type', [
   z.object({
     type: z.literal('market_face'),
     data: z.object({
+      emoji_package_id: ZInt32.describe('市场表情包 ID'),
+      emoji_id: ZInt32.describe('市场表情 ID'),
+      key: ZString.describe('市场表情 Key'),
+      summary: ZString.describe('市场表情预览文本'),
       url: ZString.describe('市场表情 URL'),
     }).describe('市场表情消息段'),
   }).describe('市场表情消息段'),
