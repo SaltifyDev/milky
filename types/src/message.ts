88322792ec3685745fc70c907b3cfcd2c7c2c169
Wei FeiYang <wesley.f.young@outlook.com>
2026-01-10 10:52:12 +0800

    feat(types): add title, preview, and summary to OutgoingForwardedMessage
    Co-authored-by: Clansty <i@gao4.pw>

--- types/src/message.ts
+++ types/src/message.ts
@@ -185,8 +185,11 @@ export const OutgoingSegment = z.discriminatedUnion('type', [
   z.object({
     type: z.literal('forward'),
     data: z.object({
+      title: ZString.nullish().describe('合并转发标题'),
+      preview: z.array(ZString).min(1).max(4).nullish().describe('合并转发预览文本，若提供，至少 1 条，至多 4 条'),
+      summary: ZString.nullish().describe('合并转发摘要'),
       get messages() {
-        return z.array(z.lazy(() => OutgoingForwardedMessage)).describe('合并转发消息段');
+        return z.array(z.lazy(() => OutgoingForwardedMessage)).describe('合并转发消息内容');
       }
     }),
   }).describe('合并转发消息段'),
