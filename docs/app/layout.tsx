89c8fc34c3afc39dfbaccf3dbd9e52ad12713fd1
Wei FeiYang <wesley.f.young@outlook.com>
2026-01-10 17:19:55 +0800

    refactor(docs): remove forced fonts from styles.css

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,16 +1,6 @@
 import { LastUpdated, Layout, Navbar } from 'nextra-theme-docs';
 import { getPageMap } from 'nextra/page-map';
 import 'nextra-theme-docs/style.css';
-import '@fontsource/inter/100.css';
-import '@fontsource/inter/200.css';
-import '@fontsource/inter/300.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-import '@fontsource/inter/800.css';
-import '@fontsource/inter/900.css';
-import '@fontsource-variable/noto-sans-sc';
 import './styles.css';
 import { commonStructs, apiCategories } from './common';
 import { Banner, Head, Search } from 'nextra/components';
