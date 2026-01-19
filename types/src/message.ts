b8e06798b5f981046a4029d9f4e680ae9bc09bf5
Wei FeiYang <wesley.f.young@outlook.com>
2026-01-19 18:29:25 +0800

    update(types): remove `@` prefix from incoming mention name

--- types/src/message.ts
+++ types/src/message.ts
@@ -23,7 +23,7 @@ export const IncomingSegment = z.discriminatedUnion('type', [
     type: z.literal('mention'),
     data: z.object({
       user_id: ZUin.describe('提及的 QQ 号'),
-      name: ZString.describe('提及的名称，由 `@` 开头'),
+      name: ZString.describe('去掉 `@` 前缀的提及的名称'),
     }),
   }).describe('提及消息段'),
 
