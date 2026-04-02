81448c078f83b45971fe55cb5ce02b225e659f12
Wei FeiYang <wesley.f.young@outlook.com>
2026-04-02 13:15:32 +0800

    fix(types): repair robust array typing

--- types/src/robust.ts
+++ types/src/robust.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import z from 'zod';
 
-export const ZRobustArray: <T extends z.ZodDiscriminatedUnion>(
-  element: T
-) => z.ZodPipe<z.ZodArray<z.ZodCatch<z.ZodLazy<T>>>> = (element) =>
-  // @ts-ignore
-  z.array(element.catch(null as any)).transform((val) => val.filter((item) => item !== null));
+export function ZRobustArray<const T extends z.ZodDiscriminatedUnion>(element: T) {
+  return z
+    .array(element.catch(null as any))
+    .transform((val) => val.filter((item) => item !== null)) as unknown as z.ZodPipe<
+    z.ZodArray<z.ZodCatch<z.ZodLazy<T>>>,
+    z.ZodArray<z.ZodLazy<T>>
+  >;
+}
