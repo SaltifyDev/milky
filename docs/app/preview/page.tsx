99ec118e63cef59d926c7279a0e8bfd5ed79e159
Wei FeiYang <wesley.f.young@outlook.com>
2026-04-12 10:41:04 +0800

    fix(docs): add metadata for preview page

--- docs/app/preview/page.tsx
+++ docs/app/preview/page.tsx
@@ -1,5 +1,10 @@
 import { Suspense } from 'react';
 import RawPreviewClient from '@/component/RawPreviewClient';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Milky | 原始文件预览',
+};
 
 export default function RawPreviewPage() {
   return (
